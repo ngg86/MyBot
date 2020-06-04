82f1613415f96855a685206cea4a7ecdb7a65fe3
Nicholas Greenslade <ng.greenslade@outlook.com>
2020-06-04 09:31:13 +0200

    Added code to Program Added code to CommandHandler

--- Business/CommandHandler.cs
+++ Business/CommandHandler.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Business
 {
-	class CommandHandler
+	public class CommandHandler
 	{
+		private readonly DiscordSocketClient _client;
+		private readonly CommandService _service;
+		private IServiceProvider _provider;
+
+		public CommandHandler(DiscordSocketClient client, CommandService service, IServiceProvider provider)
+		{
+			_client = client;
+			_service = service;
+			_provider = provider;
+		}
+
+		public async Task InitializeCommandsAsync(IServiceProvider provider)
+		{
+			_provider = provider;
+			await _service.AddModulesAsync(Assembly.GetExecutingAssembly(), _provider);
+			_client.MessageReceived += MessageReceived;
+		}
+
+		private async Task MessageReceived(SocketMessage incomingMessage)
+		{
+			if (!(incomingMessage is SocketUserMessage message)) return;
+			if (message.Source != Discord.MessageSource.User) return;
+
+			var position = 0;
+			if (!message.HasCharPrefix('!', ref position)) return;
+
+
+			var context = new SocketCommandContext(_client, message);
+			var result = await _service.ExecuteAsync(context, position, _provider);
+
+			if (result.Error.HasValue && result.Error.Value != CommandError.UnknownCommand)
+			{
+				Console.WriteLine(result.ToString());
+			}
+		}
 	}
 }
--- MyBot/Program.cs
+++ MyBot/Program.cs
@@ -1,13 +1,48 @@
-﻿using System;
+﻿using Business;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Configuration;
+using System.Threading.Tasks;
 
 namespace MyBot
 {
-	class Program
+	public class Program
 	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Hello World!");
+		private DiscordSocketClient _client;
+		private static readonly string _token = ConfigurationManager.AppSettings["token"];
+		public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
 
+		public async Task MainAsync()
+		{
+			_client = new DiscordSocketClient();
+			_client.Log += Log;
+			var services = ConfigureServices();
+
+			await services.GetRequiredService<CommandHandler>().InitializeCommandsAsync(services);
+			await _client.LoginAsync(TokenType.Bot, _token);
+			await _client.StartAsync();
+
+			await Task.Delay(-1);
 		}
+
+		#region private methods
+		private IServiceProvider ConfigureServices()
+		{
+			return new ServiceCollection()
+				.AddSingleton(_client)
+				.AddSingleton<CommandService>().
+				AddSingleton<CommandHandler>().
+				BuildServiceProvider();
+		}
+
+		private Task Log(LogMessage message)
+		{
+			Console.WriteLine(message.ToString());
+			return Task.CompletedTask;
+		}
+		#endregion
 	}
 }
